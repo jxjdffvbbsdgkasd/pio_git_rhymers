68bf474d7e87eb452abe044172ab588c8885f4c7
filip <251186@edu.p.lodz.pl>
2025-03-28 17:37:16 +0100

    wygenerowanie gettera dla total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,6 +9,10 @@ public class DefaultCountingOutRhymer {
 
     private int total = INT1;
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
